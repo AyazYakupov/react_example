fe64dc0c178a7f9677771ab1bc6f72307dffb716
AyazYakupov <a.yak.prog@gmail.com>
2020-07-12 22:53:11 +0800

    React router

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,13 +1,54 @@
 import React, { Component } from 'react'
-import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap'
+import { Navbar, NavbarBrand, Jumbotron, Collapse, Nav, NavItem, NavbarToggler } from 'reactstrap'
+import { NavLink } from 'react-router-dom'
 
 class Header extends Component {
+    constructor (props) {
+        super(props);
+        this.toggleNav = this.toggleNav.bind(this)
+        this.state = {
+            isNavOpen: false
+        }
+        
+    }
+
+    toggleNav () {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        })
+    }
+
     render () {
         return (
             <React.Fragment>
-                <Navbar dark color="primary">
+                <Navbar dark expand="md">
                     <div className="container">
-                    <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                        <NavbarToggler onClick={this.toggleNav}/>
+                        <NavbarBrand className="mr-auto" href="/"><img src='assets/images/logo.png' height="30" width="41" alt='Ristorante Con Fusion' /></NavbarBrand>
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav navbar>
+                                <NavItem>
+                                    <NavLink to="/home" className="nav-link">
+                                        <span className="fa fa-home fa-lg"> Home</span>
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink to="/aboutus" className="nav-link">
+                                        <span className="fa fa-home fa-lg"> About Us</span>
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink to="/menu" className="nav-link">
+                                        <span className="fa fa-home fa-lg"> Menu</span>
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink to="/contactus" className="nav-link">
+                                        <span className="fa fa-home fa-lg"> Contact Us</span>
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
                     </div>
                 </Navbar>
                 <Jumbotron>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import Header from './HeaderComponent'
 import Footer from './FooterComponent'
 import Menu from './MenuComponent'
+import Home from './HomeComponent'
 import { DISHES } from '../shared/dishes'
 import DishDetail from './DishdetailComponent'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
 class Main extends Component {
   constructor (props) {
@@ -17,13 +19,25 @@ class Main extends Component {
     this.setState({selectedDish: dishId})
 }
   render () {
+
+    const HomePage = () => {
+      return (
+        <Home />
+      )
+    }
+
     return (
         <div>
             <Header />
-            <div className="container">
+            <Switch>
+              <Route path='/home' component={HomePage}/>
+              <Route path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
+              <Redirect to="/home" />
+            </Switch>
+            {/* <div className="container">
                 <Menu dishes={this.state.dishes}  onClick={(dishId) => this.onDishSelect(dishId)}/>
                 <DishDetail selectedDish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-            </div>
+            </div> */}
             <Footer />
         </div>
     )
