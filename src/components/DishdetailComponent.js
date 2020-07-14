6fcee5c65b971227128d245a5b0d610bd15d21b0
AyazYakupov <a.yak.prog@gmail.com>
2020-07-15 00:17:31 +0800

    add comment form to dishdetail component

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,89 @@
-import React from 'react';
-import { Card, CardBody, CardImg, CardText, CardTitle, BreadcrumbItem, Breadcrumb } from 'reactstrap'
+import React, {Component} from 'react';
+import { Card, CardBody, CardImg, CardText, CardTitle, BreadcrumbItem, Breadcrumb, Button, Modal, ModalHeader, ModalBody, Row, Label, Col} from 'reactstrap'
 import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors} from 'react-redux-form'
+
+const required = (val) => val && val.length;
+const minLength = (len) => (val) => val && (val.length >= len);
+const maxLength = (len) => (val) => val && (val.length <= len);
+
+class CommentForm extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            isModalOpen: false
+        };
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    toggleModal(){
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        })
+    }
+
+    handleSubmit(values){
+        console.log('Current State is: ' + JSON.stringify(values));
+        alert('Current State is: ' + JSON.stringify(values));
+    }
+
+    render () {
+        return (
+            <React.Fragment>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                        Submit Comment
+                    </ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={(values) => {this.handleSubmit(values)}}>
+                            <Row className="form-group">
+                                <Col>
+                                    <Label htmlFor="rating">Rating</Label>
+                                    <Control.select model=".rating" name="rating" id="rating" className="form-control">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </Control.select>
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col>
+                                    <Label htmlFor="author">Your Name</Label>
+                                    <Control.text model=".author" name="author" id="author" className="form-control" placeholder="Your Name" 
+                                    validators={{required, minLength: minLength(3), maxLength: maxLength(15)}}/>
+                                    <Errors className="text-danger" model=".author" show="touched" 
+                                    messages={{
+                                        required: "Required",
+                                        minLength: 'Must be greater than 2 characters',
+                                        maxLength: 'Must be 15 characters or less'
+                                    }}/>
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col>
+                                    <Label htmlFor="comment">Comment</Label>
+                                    <Control.textarea model=".comment" name="comment" id="comment" className="form-control" rows="6"/>
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col>
+                                    <Button type="submit" color="primary">
+                                        Submit
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+                <Button outline color="secondary" onClick={this.toggleModal}><span className="fa fa-pencil fa-lg"></span> Submit Comment</Button>
+            </React.Fragment>
+        )
+    }
+    
+}
 
 function RenderDish ({dish}) {
     return (
@@ -13,6 +96,8 @@ function RenderDish ({dish}) {
         </Card>
     )
 }
+
+
 function RenderComments({comments}) {
     if (comments != null) {
         return (
@@ -29,6 +114,7 @@ function RenderComments({comments}) {
                         )
                     })}
                 </ul>
+                <CommentForm /> 
             </div>
         )
     }
@@ -38,16 +124,16 @@ function RenderComments({comments}) {
         )
     }
 }
-const DishDetail = (props) => {
-
+function DishDetail (props) {
     return (
         <div className="container">
+            
             <div className="row">
                 <Breadcrumb>
                     <BreadcrumbItem>
                         <Link to="/menu">Menu</Link>
                     </BreadcrumbItem>
-                    <BreadcrumbItem active>props.dish.name</BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
                 </Breadcrumb>
                 <div className="col-12">
                     <h3>{props.dish.name}</h3>
@@ -56,11 +142,12 @@ const DishDetail = (props) => {
             </div>
             <div className="row">
                 <div className="col-12 col-md-5 m-1"><RenderDish dish={props.dish}/></div>
-                <div className="col-12 col-md-5 m-1"><RenderComments comments={props.comments}/></div>
+                <div className="col-12 col-md-5 m-1">
+                    <RenderComments comments={props.comments}/>
+                </div>
             </div>
         </div>
-
-        )
+    )
 
 }
 
