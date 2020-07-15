7372ac5f769a9458130cc86c48d1de547095a27c
AyazYakupov <a.yak.prog@gmail.com>
2020-07-15 22:47:43 +0800

    react redux from revisited

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -5,6 +5,8 @@ import {Promotions} from './promotions';
 import {Leaders} from './leaders';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { createForms } from 'react-redux-form'
+import {InitialFeedback} from './forms'
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -12,7 +14,8 @@ export const ConfigureStore = () => {
             dishes: Dishes,
             promotions: Promotions,
             leaders: Leaders,
-            comments: Comments
+            comments: Comments,
+            ...createForms({feedback: InitialFeedback})
         }),
         applyMiddleware(thunk, logger)
     )
